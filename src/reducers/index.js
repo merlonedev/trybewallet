76154f68e3aa1e73990a185de028131b8165778c
Sass <dudu_sass12@hotmail.com>
2021-07-30 14:44:27 -0300

    Iniciando o projeto, vamos com tudo ! :rocket:

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,26 @@
 // import user from './user';
 // import wallet from './wallet';
+const initialState = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: []
+  }
+}
 
+const taskReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_STATE: {
+      return {
+        ...state,
+        user: [...state.user, action.payload],
+        wallet: [...state.wallet, action.payload],
+      };
+    }
+    default: return state;
+  }
+};
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
