9d0445be811ed96ec446a32bc6003ec285e31b9d
zLibrian <kevinwillian084@gmail.com>
2021-08-12 17:52:11 -0300

    Add store

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Logins</div>;
+    const { email, password } = this.state;
+    return (
+      <form>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            onChange={ this.handleChange }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Password:
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            onChange={ this.handleChange }
+            value={ password }
+          />
+        </label>
+        <button type="button">
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
