e0ab9861966a98f960459415a1c44a7ce4e4b1c6
Josimar Schneider <josimarschneider@gmail.com>
2021-08-12 19:29:05 -0400

    1. Crie uma página inicial de login

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// preparando ambiente
+// preparando ambientegi
 
 ReactDOM.render(
   <BrowserRouter>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
+import Input from '../Components/Input';
+
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <Input
+          label="Email"
+          name="email"
+          data-testid="email-input"
+          type="email"
+          value={ email }
+          onChange={ () => {} }
+        />
+        <Input
+          label="Senha"
+          name="senha"
+          data-testid="password-input"
+          type="senha"
+          value={ password }
+          onChange={ () => {} }
+        />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
