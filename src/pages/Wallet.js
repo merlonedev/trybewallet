486b290ec8ea5a95d225b89ddca90399d592f290
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-08-12 16:50:07 -0300

    Requisito 9

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import CostsForm from '../components/CostsForm';
+import Expenses from '../components/Expenses'
 
 class Wallet extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <CostsForm />
+        <Expenses />
       </div>
     );
   }
