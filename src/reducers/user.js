86f9bf30f23b4ce4fd2258c9034943ad8edb1f03
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-08-12 22:56:55 -0300

    Cria reducer de usuario

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,25 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  const { type, email } = action;
+  switch (type) {
+  case LOGIN:
+    return {
+      ...state,
+      user: {
+        email,
+      },
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default user;
