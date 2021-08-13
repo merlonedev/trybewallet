64f2896717fc3813738c6841d747588ac9781564
Djaniza Vasques <djanizav@gmail.com>
2021-08-12 23:23:29 -0300

    Corrige estilo da pagina de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { actGetEmail } from '../actions';
 import loginImage from '../images/loginimage.jpg';
-import '../loginpage.css';
 
 class Login extends React.Component {
   constructor() {
