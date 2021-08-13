ed469c88eca97514317d0d3125cb59b928843e0b
LuanAlexandre <luanrb19@gmail.com>
2021-08-12 21:03:42 -0400

    Dia 16.6 - projeto-trybe-wallet: requisito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 export default class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,93 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      disable: true,
+    };
+
+    this.activateLoginButton = this.activateLoginButton.bind(this);
+    this.runSaveEmailAndActivateLoginButton = this
+      .runSaveEmailAndActivateLoginButton.bind(this);
+    this.runSavePasswordAndActivateLoginButton = this
+      .runSavePasswordAndActivateLoginButton.bind(this);
+  }
+
+  async saveFieldValue(field, value) {
+    this.setState({ [field]: value });
+  }
+
+  verifyPasswordLength(password) {
+    if (password) {
+      const passwordMinimumLength = 6;
+      return password.length >= passwordMinimumLength;
+    }
+  }
+
+  verifyEmailFormat(email) {
+    if (email) {
+      const firstVerification = email.includes('@');
+      const secondVerification = email.endsWith('.com');
+      return firstVerification && secondVerification;
+    }
+  }
+
+  activateLoginButton() {
+    const { emailInput, passwordInput } = this.state;
+    const isBiggerThan = this.verifyPasswordLength(passwordInput);
+    const isValidEmail = this.verifyEmailFormat(emailInput);
+    if (isBiggerThan && isValidEmail) {
+      this.setState({
+        disable: false,
+      });
+    } else {
+      this.setState({
+        disable: true,
+      });
+    }
+  }
+
+  async runSaveEmailAndActivateLoginButton(field, value) {
+    await this.saveFieldValue(field, value);
+    this.activateLoginButton();
+  }
+
+  async runSavePasswordAndActivateLoginButton(field, value) {
+    await this.saveFieldValue(field, value);
+    this.activateLoginButton();
+  }
+
   renderLoginFields() {
+    const { disable } = this.state;
     return (
       <form>
-        <input data-testid="email-input" type="email" placeholder="e-mail" />
-        <input data-testid="password-input" type="password" placeholder="password" />
-        <button type="button">Entrar</button>
+        <input
+          data-testid="email-input"
+          name="emailInput"
+          type="email"
+          placeholder="e-mail"
+          onChange={ ({ target }) => this
+            .runSaveEmailAndActivateLoginButton(target.name, target.value) }
+        />
+        <input
+          data-testid="password-input"
+          name="passwordInput"
+          type="password"
+          placeholder="password"
+          onChange={ ({ target }) => this
+            .runSavePasswordAndActivateLoginButton(target.name, target.value) }
+        />
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ disable }
+          >
+            Entrar
+          </button>
+        </Link>
       </form>
     );
   }
