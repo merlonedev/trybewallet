d64c43718f13dafa407cc8e9f62281ff7503dd76
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-08-14 11:57:16 -0300

    cria reducer wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,35 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { FETCH_ERROR, FETCH_SUCCESS, FETCHING } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  isFetching: false,
+  error: '',
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  const { type, error, response, isFetching } = action;
+  switch (type) {
+  case FETCHING:
+    return {
+      ...state,
+      isFetching,
+    };
+  case FETCH_SUCCESS:
+    return {
+      ...state,
+      isFetching,
+      currencies: [...response],
+    };
+  case FETCH_ERROR:
+    return {
+      ...state,
+      isFetching,
+      error,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
