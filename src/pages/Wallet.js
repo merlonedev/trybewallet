5cbaffd4526d57b4c977cafa1c0877ca61ca5ab6
Janderson Soares <jandinsm@gmail.com>
2021-08-15 19:00:41 -0300

    Desafio 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -11,6 +11,42 @@ class Wallet extends React.Component {
         <span data-testid="email-field">{ email }</span>
         <p data-testid="total-field">0</p>
         <p data-testid="header-currency-field">BRL</p>
+        <form>
+          <label htmlFor="value">
+            Valor
+            <input id="value" type="text" name="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input id="description" type="text" name="description" />
+          </label>
+          <label htmlFor="coin">
+            Moeda
+            <select name="coin" id="coin">
+              <option>Opção vazia</option>
+              <option>Opção vazia</option>
+              <option>Opção vazia</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento
+            <select name="payment" id="payment">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select name="tag" id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
