f4dcaf8ad77be42e0f77bc3cd388499da6c5227f
Janderson Soares <jandinsm@gmail.com>
2021-08-15 18:01:44 -0300

    usando e aprendendo mapstatetoprops requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>Carteira</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <h1>Carteira</h1>
+        <span data-testid="email-field">{ email }</span>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
