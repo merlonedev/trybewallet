ce3da9b2a585c9d952cc23fcd18e37f222e9f51b
Antonio Malato <malatoneto@gmail.com>
2021-08-15 23:58:48 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      redirect: false,
+    };
+    this.handlerChange = this.handlerChange.bind(this);
+    this.validationLogin = this.validationLogin.bind(this);
+  }
+
+  handlerChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    }, this.validationLogin);
+  }
+
+  // https://stackoverflow.com/questions/32004990/javascript-regular-expression-validation
+  validationLogin() {
+    const { email, password } = this.state;
+    const numberValidation = 6;
+    const regexValidation = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const regexEmail = regexValidation.test(email);
+
+    if (password.length >= numberValidation && regexEmail) {
+      this.setState({ redirect: true });
+    } else (this.setState({ redirect: false }));
+  }
+
   render() {
+    const { redirect } = this.state;
     return (
       <>
         <form>
@@ -11,6 +41,7 @@ class Login extends Component {
               name="email"
               type="email"
               data-testid="email-input"
+              onChange={ this.handlerChange }
             />
           </label>
           <label htmlFor="password-input">
@@ -18,11 +49,13 @@ class Login extends Component {
               name="password"
               type="password"
               data-testid="password-input"
+              onChange={ this.handlerChange }
             />
           </label>
         </form>
         <button
           type="button"
+          disabled={ !redirect }
         >
           Entrar
         </button>
