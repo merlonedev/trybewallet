c0d227b85affdcf9e55b337cdd22c9463a6341aa
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-08-16 10:28:14 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <h1>Login</h1>
+        <label htmlFor="email">
+          Email:
+          <input type="email" id="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input type="password" id="password" data-testid="password-input" />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
 export default Login;
-// !
