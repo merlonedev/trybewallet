ede88d8d58db4944479bc3b446cec0100b660664
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-16 11:54:46 -0300

    implementa actions para buscar API das moedas

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,4 +2,22 @@
 
 export const addEmail = (email) => ({ type: 'ADD_EMAIL', email });
 
-export const addCurrencies = (currency) => ({ type: 'ADD_CURRENCY', currency });
+export const getCurrency = () => ({
+  type: 'GET_CURRENCY',
+});
+
+export const getCurrencySucess = (currencies) => ({
+  type: 'GET_CURRENCY_SUCESS', currencies,
+});
+
+export const getCurrencyError = (error) => ({
+  type: 'GET_CURRENCY_ERROR', error,
+});
+
+export const fetchCurrency = () => (dispatch) => {
+  dispatch(getCurrency());
+  fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((response) => dispatch(getCurrencySucess(response)))
+    .catch((error) => dispatch(getCurrencyError(error)));
+};
