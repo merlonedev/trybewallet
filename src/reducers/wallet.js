79b8909f767719b530e29ddf7b2382cb1ac08e92
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-16 12:02:32 -0300

    implementa reducers

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -2,16 +2,24 @@
 const INICIA_STATE = {
   currencies: [],
   expenses: [],
+  error: '',
+  total: 0,
 };
 
 const wallet = (state = INICIA_STATE, action) => {
   switch (action.type) {
-  case 'ADD_CURRENCY': {
-    return {
+  case 'GET_CURRENCY':
+    return state;
+  case 'GET_CURRENCY_SUCESS':
+    return ({
       ...state,
-      currencies: action.currency,
-    };
-  }
+      currencies: [action.currencies],
+    });
+  case 'GET_CURRENCY_ERROR':
+    return ({
+      ...state,
+      error: action.error,
+    });
   default:
     return state;
   }
