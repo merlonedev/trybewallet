231e38c1af3c886d27f37f4b042e76a4dc61c75b
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-16 11:39:04 -0300

    implementa thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import rootReduce from '../reducers';
 
-const store = createStore(rootReduce);
+const store = createStore(rootReduce, applyMiddleware(thunk));
 
 export default store;
