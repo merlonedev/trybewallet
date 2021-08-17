18b15b16eb3a8766830dc9fe4c607a57de11d4f1
Alice Gonçalves <galice9992@gmail.com>
2021-08-17 15:12:23 -0300

    resolve problemas com lint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Routes from './Routes';
 
-class App extends React.Component{
+class App extends React.Component {
   render() {
     return (
-    <Routes />
+      <Routes />
     );
   }
 }
