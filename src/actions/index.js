defbff30b61719c2fbc3142193cbffb91885eb57
martinsph <ph.mmartins@gmail.com>
2021-08-17 19:47:54 -0300

    add req 5 e 6

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
-export const USER_EMAIL = 'user-email';
-export const ADD_EXPENSE = 'add-expense';
-export const DELETE_EXPENSE = 'delete-expense';
+export const USER_EMAIL = 'user_email';
+export const ADD_EXPENSE = 'add_expense';
+export const DELETE_EXPENSE = 'delete_expense';
 export const CURRENCY = 'currency';
 
 export const saveEmail = (actualEmail) => ({
@@ -19,3 +19,20 @@ export const addCurrency = (currency) => ({
   type: CURRENCY,
   currency,
 });
+
+export const deleteExpense = (id) => ({ type: DELETE_EXPENSE, id });
+
+// mudar
+export const fetchApi = (expenses) => async (dispatch) => {
+  const URL = 'https://economia.awesomeapi.com.br/json/all';
+
+  const fetchApiResquest = await fetch(URL);
+  const jsonResponse = await fetchApiResquest.json();
+
+  if (!expenses) {
+    const currents = Object.keys(jsonResponse)
+      .filter((current) => current !== 'USDT');
+    return dispatch(getCurrencies(currents));
+  }
+  dispatch(addExpense({ ...expenses, exchangeRates: jsonResponse }));
+};
