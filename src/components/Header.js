6ba92be66200af0fdb72e6cd9cc9e572e33e210b
Alice Gonçalves <galice9992@gmail.com>
2021-08-17 15:55:54 -0300

    resolve requisito 5, adiciona header da wallet

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
