6dec387cf23e2326fe141520355dc4165bdcc1e7
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-08-16 23:02:46 -0300

    tenta N2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,74 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disabled: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleLogin() {
+    const { email, password } = this.state;
+    const emailLogin = email.split('@').length === 2 && email.split('.com')[1] === '';
+    const lengthPass = 6;
+
+    if (emailLogin && password.length > lengthPass) {
+      this.setState({
+        disabled: true,
+      });
+    } else {
+      this.setState({
+        disabled: false,
+      });
+    }
+  }
+
   render() {
+    const { email, password, disabled } = this.state;
     return (
       <form>
         <h1>Login</h1>
         <label htmlFor="email">
           Email:
-          <input type="email" id="email" data-testid="email-input" />
+          <input
+            type="email"
+            id="email"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChange }
+            onKeyUp={ this.handleLogin }
+          />
         </label>
         <label htmlFor="password">
           Senha:
-          <input type="password" id="password" data-testid="password-input" />
+          <input
+            type="password"
+            id="password"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+            onKeyUp={ this.handleLogin }
+          />
         </label>
-        <button type="button">Entrar</button>
+        <button
+          type="button"
+          disabled={ !disabled }
+        >
+          Entrar
+        </button>
       </form>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+import Wallet from '../pages/Wallet';
 import user from './user';
 
-const rootReducer = combineReducers({ user });
+const rootReducer = combineReducers({
+  user,
+  Wallet,
+});
 
 export default rootReducer;
