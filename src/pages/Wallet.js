ee77f70b930389c207a22086de16e2c20a6f3681
Pedro Resck <pedroresck1@gmail.com>
2021-08-16 22:28:14 -0300

    Requisito 5 - cria um header para o wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header>
+          <p data-testid="email-field">{`E-mail: ${email}`}</p>
+          <p data-testid="total-field">Gasto total: 0 </p>
+          <p data-testid="header-currency-field">Câmbio atual: BRL </p>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({ email: state.user.email });
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
