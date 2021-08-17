32ca1edde846996c47fc23de83ca0c8c9e403bf2
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-08-16 21:35:28 -0300

    bora pra cima,wallet! :rocket:

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <h1>Login</h1>
+        <label htmlFor="email">
+          Email:
+          <input type="email" id="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input type="password" id="password" data-testid="password-input" />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
