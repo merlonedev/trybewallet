01a7f11bb703f53ccfe7cea86f9350394923d083
gscuri <gscuri6@hotmail.com>
2021-08-17 01:19:39 -0300

    requisito 8 finalmente!!!!!!!

--- src/components/expensesForm/ExpenseDescription.js
+++ src/components/expensesForm/ExpenseDescription.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ExpenseDescription extends Component {
   render() {
@@ -19,5 +20,8 @@ class ExpenseDescription extends Component {
     );
   }
 }
-
+ExpenseDescription.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  description: PropTypes.string.isRequired,
+};
 export default ExpenseDescription;
