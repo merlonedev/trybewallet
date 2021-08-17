8f8c4bcefb64ce5ff6a576f0d9349d3cde86974b
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-17 11:27:07 -0300

    Valida email e senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      enableToLogin: false,
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleValidate() {
+    const { email, password } = this.state;
+    const regexChecker = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const minPasswordLength = 6;
+    return (regexChecker.test(email) && password.length >= minPasswordLength);
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <main>
         <h1>Trybe-Wallet</h1>
@@ -11,6 +37,9 @@ class Login extends React.Component {
           <input
             type="text"
             data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ this.handleInput }
           />
         </label>
         <br />
@@ -20,11 +49,15 @@ class Login extends React.Component {
           <input
             type="password"
             data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ this.handleInput }
           />
         </label>
         <br />
         <button
           type="button"
+          disabled={ !this.handleValidate() }
         >
           ENTRAR
         </button>
