e0a44eb4df0a17a8fd90da7a220c8c1ae36901b9
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-08-17 18:11:03 -0300

    Updates

---
+++ precache-manifest.dab02f7df4fa04d34dc9422b1f090104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617038e85aeb68798ea0aa168074575c",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "da5ba6f7ea84f55a7590",
+    "url": "/wallet/static/css/main.b063da70.chunk.css"
+  },
+  {
+    "revision": "04e90df21aa47afdcba6",
+    "url": "/wallet/static/js/2.f49a68bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet/static/js/2.f49a68bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5ba6f7ea84f55a7590",
+    "url": "/wallet/static/js/main.765b8034.chunk.js"
+  },
+  {
+    "revision": "4fb4cea7ccc09b9511f8",
+    "url": "/wallet/static/js/runtime-main.8f1525cf.js"
+  }
+]);
