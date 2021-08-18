333c7e27d3362919254a24a83363a6f39db0bdf4
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-18 11:34:26 -0300

    Cria o formulário

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,25 +6,60 @@ class Wallet extends React.Component {
   render() {
     const { userEmail } = this.props;
     return (
-      <header>
-        <h1>Bem-Vindo(a) à Trybe Wallet</h1>
-        <p
-          data-testid="email-field"
-        >
-          Email:
-          { userEmail }
-        </p>
-        <p
-          data-testid="total-field"
-        >
-          Despesa total: R$ 0,00
-        </p>
-        <p
-          data-testid="header-currency-field"
-        >
-          Câmbio utilizado: BRL
-        </p>
-      </header>
+      <main>
+        <header>
+          <h1>Bem-Vindo(a) à Trybe Wallet</h1>
+          <p
+            data-testid="email-field"
+          >
+            Email:
+            { userEmail }
+          </p>
+          <p
+            data-testid="total-field"
+          >
+            Despesa total: R$ 0,00
+          </p>
+          <p
+            data-testid="header-currency-field"
+          >
+            Câmbio utilizado: BRL
+          </p>
+        </header>
+        <form>
+          <label>
+            Valor:
+            <input />
+          </label>
+          <label>
+            Descrição:
+            <input />
+          </label>
+          <label>
+            Moeda
+            <select>
+            </select>
+          </label>
+          <label>
+            Método de pagamento
+            <select>
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label>
+            Tag
+            <select>
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+      </main>
     );
   }
 }
