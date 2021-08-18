7be8389f939c654afafcc976b26fb5217ce91f8a
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-08-18 10:46:26 -0300

    Estruturação básica da pagina de Login, actions, reducers e store.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
-// Coloque aqui suas actions
+export const userLogin = (email) => ({
+  type: USER_LOGIN,
+  payload: {
+    email,
+  },
+});
+
+export const newCurrencie = (currencie) => ({
+  type: 'NEW_CURRENCIE',
+  payload: {
+    currencie,
+  },
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // email: '',
+      // password: '',
+      disable: true,
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { disable } = this.state;
+    const { dispatchLogin } = this.props;
+    return (
+      <div>
+        Login
+        <label htmlFor="email">
+          <input
+            id="email"
+            data-testid="email-input"
+            type="text"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            id="password"
+            data-testid="password-input"
+            type="password"
+            minLength="6"
+          />
+        </label>
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ disable }
+            onClick={ () => dispatchLogin(email) }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchLogin: (email) => dispatch(userLogin(email)),
+});
+
+Login.propTypes = {
+  dispatchLogin: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
