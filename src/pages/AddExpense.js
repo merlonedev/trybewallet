4c69312b1d6aa396eea08b1c0be8fe204eb7c496
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-08-18 13:53:45 -0300

    Resolve requisitos 6 ao 8

--- src/pages/AddExpense.js
+++ src/pages/AddExpense.js
@@ -7,34 +7,64 @@ class AddExpense extends Component {
   constructor() {
     super();
     this.state = {
-      // value: 0,
-      // description: '',
-      // currency: 'USD',
-      // tag: 'Alimentação',
+      value: 0,
+      description: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
     };
+    this.handleChangeAddInput = this.handleChangeAddInput.bind(this);
+    this.valueAndDescribe = this.valueAndDescribe.bind(this);
+    this.payAndTag = this.payAndTag.bind(this);
   }
 
-  render() {
+  componentDidMount() {
+    const { recieveCurrency } = this.props;
+    recieveCurrency();
+  }
+
+  handleChangeAddInput({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  valueAndDescribe() {
     return (
       <form>
         <label htmlFor="value">
-          Valor:
-          <input type="text" name="name" />
+          Valor
+          <input
+            type="number"
+            name="value"
+            id="value"
+            onChange={ this.handleChangeAddInput }
+          />
         </label>
         <label htmlFor="description">
-          Descrição:
-          <input type="text" name="name" />
+          Descrição
+          <input
+            type="text"
+            name="description"
+            id="description"
+            onChange={ this.handleChangeAddInput }
+          />
         </label>
-        <label htmlFor="moeda">
-          Moeda:
-          <input type="select" name="name" />
-        </label>
+      </form>
+    );
+  }
+
+  payAndTag() {
+    return (
+      <form>
         <label htmlFor="method">
           Método de pagamento
           <select
             type="select"
             name="method"
             id="method"
+            onChange={ this.handleChangeAddInput }
           >
             <option>Dinheiro</option>
             <option>Cartão de crédito</option>
@@ -47,6 +77,7 @@ class AddExpense extends Component {
             type="select"
             name="tag"
             id="tag"
+            onChange={ this.handleChangeAddInput }
           >
             <option>Alimentação</option>
             <option>Lazer</option>
@@ -58,7 +89,46 @@ class AddExpense extends Component {
       </form>
     );
   }
+
+  render() {
+    const { mapCurrencies, thisExpense } = this.props;
+    delete mapCurrencies.USDT;
+    const { value, description, method, tag } = this.state;
+    return (
+      <>
+        {this.valueAndDescribe()}
+        <form>
+          <label htmlFor="currency">
+            Moeda
+            <select
+              type="select"
+              name="currency"
+              id="currency"
+              onChange={ this.handleChangeAddInput }
+            >
+              {Object.values(mapCurrencies)
+                .map((currency, index) => <option key={ index }>{currency.code}</option>)}
+            </select>
+          </label>
+        </form>
+        {this.payAndTag()}
+        <button
+          type="reset"
+          onClick={ () => {
+            thisExpense(this.state);
+          } }
+        >
+          Adicionar despesa
+        </button>
+        <p>{value}</p>
+        <p>{description}</p>
+        <p>{method}</p>
+        <p>{tag}</p>
+      </>
+    );
+  }
 }
+
 const mapDispatchToProps = (dispatch) => ({
   recieveCurrency: () => dispatch(requestCurrency()),
   thisExpense: (expenses) => dispatch(addExpense(expenses)),
