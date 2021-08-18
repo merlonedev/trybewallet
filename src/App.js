cc749197fde1d3df39eb1281db2c423b4f7e07aa
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-18 11:08:30 -0300

    Cria a rota para a carteira

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Switch>
+          <Route path="/carteira">
+            <Wallet />
+          </Route>
           <Route path="/">
             <Login />
           </Route>
