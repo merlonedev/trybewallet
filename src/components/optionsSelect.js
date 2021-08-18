49c69c158c9780fee881e570e4f5f21f74286388
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-08-18 02:01:03 -0300

    cria array de opçoes para o componente select

---
+++ src/components/optionsSelect.js
@@ -0,0 +1,35 @@
+export const methods = [
+  {
+    val: 'cash',
+    lab: 'Dinheiro',
+  },
+  {
+    val: 'cred',
+    lab: 'Cartão de crédito',
+  },
+  {
+    val: 'debt',
+    lab: 'Cartão de débito',
+  }];
+
+export const category = [
+  {
+    val: 'food',
+    lab: 'Alimentação',
+  },
+  {
+    val: 'laze',
+    lab: 'Lazer',
+  },
+  {
+    val: 'wook',
+    lab: 'Trabalho',
+  },
+  {
+    val: 'trans',
+    lab: 'Transporte',
+  },
+  {
+    val: 'health',
+    lab: 'Saúde',
+  }];
