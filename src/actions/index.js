b6b205a894a2218edcec42aba8f8439a9ff69aee
Gabriel <gribeiro237@gmail.com>
2021-08-18 17:05:24 -0300

    se errar e humano, eu nasci com muita humanidade: corrigindo requisito 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 export const ADD_USER = 'ADD_USER';
 
 export const saveUserInfo = (value) => ({
-  type: ADD_EMAIL, payload: value,
+  type: ADD_USER, payload: value,
 });
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { func } from 'prop-types';
 import { saveUserInfo } from '../actions';
 
 class Login extends React.Component {
