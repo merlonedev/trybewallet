036f6d8bc31fa570f48d217d681eef727fd7530f
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-08-18 20:05:58 -0300

    requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Moedas from './Moedas';
 
 class Wallet extends React.Component {
   render() {
+    const { email } = this.props;
     return (
       <div>
+        <header>
+          <p data-testid="email-field">{ email }</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
         TrybeWallet
         <form>
           <label htmlFor="despesas">
@@ -39,4 +47,12 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
