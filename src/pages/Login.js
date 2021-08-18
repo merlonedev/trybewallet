5c6dc5f77393d8fb6b72532abea26ad49828cff5
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-08-17 22:52:42 -0300

    Requisito 1 ao 3 - Meu projeto da Turma 11

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,105 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveLoginName } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      activateLogin: true,
+      shouldRedirectToCarteira: false,
+    };
+    this.handleChangeInLoginInputs = this.handleChangeInLoginInputs.bind(this);
+    this.activateLoginBtn = this.activateLoginBtn.bind(this);
+    this.letsStart = this.letsStart.bind(this);
+  }
+
+  handleChangeInLoginInputs({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.activateLoginBtn();
+  }
+
+  letsStart() {
+    const { importLogin } = this.props;
+    const { email } = this.state;
+    importLogin(email);
+    this.setState({
+      shouldRedirectToCarteira: true,
+    });
+  }
+
+  activateLoginBtn() {
+    const { email, password } = this.state;
+    const minimumPw = 5;
+    const checkEmail = /.+@.+\.[A-Za-z]+$/;
+    if (password.length >= minimumPw && checkEmail.test(email)) {
+      this.setState({
+        activateLogin: false,
+      });
+    } else {
+      this.setState({
+        activateLogin: true,
+      });
+    }
+  }
+
   render() {
-    return <p>Teste</p>;
+    const { email, password, activateLogin, shouldRedirectToCarteira } = this.state;
+
+    if (shouldRedirectToCarteira) {
+      return <Redirect to="/carteira" />;
+    }
+
+    return (
+      <div>
+        <label htmlFor="email">
+          E-mail:
+          <input
+            data-testid="email-input"
+            value={ email }
+            type="text"
+            id="email"
+            name="email"
+            onChange={ this.handleChangeInLoginInputs }
+          />
+
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            data-testid="password-input"
+            value={ password }
+            type="password"
+            id="password"
+            name="password"
+            onChange={ this.handleChangeInLoginInputs }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ activateLogin }
+          onClick={ this.letsStart }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  importLogin: (state) => dispatch(saveLoginName(state)),
+});
+
+Login.propTypes = {
+  importLogin: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SAVE_LOGIN_NAME } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case SAVE_LOGIN_NAME:
+    return { ...state, email: action.payload };
+  default:
+    return state;
+  }
+}
+
+export default user;
