c82ebc93de5c722f89f8424d46773951d94f18e6
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-08-18 12:54:07 -0300

    Feat: Setting Login and Wallet Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      // <BrowserRouter>
+      <div>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </div>
+      // </BrowserRouter>
+    );
+  }
 }
 
 export default App;
