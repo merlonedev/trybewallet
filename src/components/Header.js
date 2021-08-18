ddbfece80b918037e13095b23a5b53b2cc6e3ebe
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-08-18 01:58:10 -0300

    add logica para renderizar o total de despesas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,55 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.setTotalField = this.setTotalField.bind(this);
+  }
+
+  setTotalField() {
+    const { expenses } = this.props;
+    if (expenses.length > 0) {
+      const totalField = expenses.reduce((acc, curr) => {
+        const value = parseFloat(curr.expense);
+        const { coin, exchangeRates } = curr;
+        const convert = exchangeRates[coin].ask * value;
+        return acc + convert;
+      }, 0);
+      return totalField;
+    }
+    return 0;
+  }
+
   render() {
-    // const { props: { userEmail, totalField, currency } } = this;
+    const { props: { userEmail }, setTotalField } = this;
     return (
       <header>
         <aside>
-          <h2 data-testid="email-field">oi mo</h2>
-          <h3 data-testid="total-field">oi mo</h3>
-          <h4 data-testid="header-currency-field">oi mo </h4>
+          <h2 data-testid="email-field">
+            {`E-mail: ${userEmail}`}
+          </h2>
+          <h3 data-testid="total-field">
+            {`Total de despesas: ${setTotalField()}`}
+          </h3>
+          <h4 data-testid="header-currency-field">BRL</h4>
         </aside>
       </header>
     );
   }
 }
 
-export default Header;
-
-/* const { string, number } = PropTypes;
+const { string, arrayOf, objectOf } = PropTypes;
 
 Header.propTypes = {
   userEmail: string.isRequired,
-  totalField: number.isRequired,
-  currency: string.isRequired,
+  expenses: arrayOf(objectOf(string)).isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  userEmail: state.user,
-  totalField: state.wallet,
-  currency: state.wallet,
+  userEmail: state.user.email,
+  expenses: state.wallet.expenses,
+});
 
-}); */
-// connect(mapStateToProps)(Header)
+export default connect(mapStateToProps)(Header);
