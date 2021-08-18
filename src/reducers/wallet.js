db1fa12140302184015d56d8fac66dcc4ec0b348
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-08-17 22:42:22 -0300

    Feat: Wallet state

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,33 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { CURRENCIES_ACTION, EXPENSES_ACTION } from '../actions';
+
+const INITIAL_STATE = {
+
+  currencies: [],
+  expenses: [],
+
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case CURRENCIES_ACTION:
+    return (
+      {
+        ...state,
+        currencies: action.email,
+      }
+    );
+  case EXPENSES_ACTION:
+    return (
+      {
+        ...state,
+        expenses: action.expenses,
+      }
+    );
+  default:
+    return state;
+  }
+};
+
+export default wallet;
