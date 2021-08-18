c00346c952ffe679f3a7c5584581753616d100c5
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-08-17 22:41:05 -0300

    Feat: Create all actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,25 @@
 // Coloque aqui suas actions
+export const USER_ACTION = 'USER_ACTION';
+export const CURRENCIES_ACTION = 'CURRENCIES_ACTIONS';
+export const EXPENSES_ACTION = 'EXPENSES_ACTION';
+
+export const userAction = (email) => (
+  {
+    type: USER_ACTION,
+    email,
+  }
+);
+
+export const currenciesAction = (currencies) => (
+  {
+    type: CURRENCIES_ACTION,
+    currencies,
+  }
+);
+
+export const expensesAction = (expenses) => (
+  {
+    type: EXPENSES_ACTION,
+    expenses,
+  }
+);
