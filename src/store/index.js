78ec84883f63eeebda484528edaa98503bb1b4e2
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-08-18 12:56:04 -0300

    Fix: Modify devtools extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducer from '../reducers';
 
-const store = createStore(rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
 
 export default store;
