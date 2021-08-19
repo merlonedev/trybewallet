d4d328253dc5d931d9a62822f160f397bfe68021
André Dantas <aluizcfd@gmail.com>
2021-08-19 18:14:13 -0300

    Adição de uma "logo" na página de Login, adição de placeholders nos inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import logo from '../wallet.png';
 
 class Login extends React.Component {
   render() {
     return (
       <form>
-        <img alt="logo Trybe" />
+        <img src={ logo } width="50" alt="logo Trybe" />
         <input
           data-testid="email-input"
           type="email"
+          placeholder="Email"
         />
         <input
           data-testid="password-input"
           type="password"
+          placeholder="Senha"
         />
         <button
           type="submit"
