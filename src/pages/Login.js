7bcbf3031555859da9d9406ce0ffa9e99425daed
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-08-19 20:44:14 -0300

    the buton login is disabled when a invalid email is typing and enter the page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveLoginName } from '../actions';
 
 class Login extends Component {
   constructor() {
@@ -6,11 +10,51 @@ class Login extends Component {
     this.state = {
       email: '',
       password: '',
+      activateLogin: true,
     };
+    this.handleChangeInLogin = this.handleChangeInLogin.bind(this);
+    this.letsStart = this.initStart.bind(this);
+  }
+
+  handleChangeInLogin({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.activateLogin();
+  }
+
+  initStart() {
+    const { importLogin } = this.props;
+    const { email } = this.state;
+    importLogin(email);
+    this.setState({
+      shouldRedirectToCarteira: true,
+    });
+  }
+
+  activateBtnLogin() {
+    const { email, password } = this.state;
+    const minimumPw = 5;
+    const checkEmail = /.+@.+\.[A-Za-z]+$/;
+    if (password.length >= minimumPw && checkEmail.test(email)) {
+      this.setState({
+        activateLogin: false,
+      });
+    } else {
+      this.setState({
+        activateLogin: true,
+      });
+    }
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, activateLogin,
+      shouldRedirectToCarteira } = this.state;
+
+    if (shouldRedirectToCarteira) {
+      return <Redirect to="/carteira" />;
+    }
     return (
       <div>
         <form>
@@ -21,6 +65,7 @@ class Login extends Component {
               type="email"
               value={ email }
               data-testid="email-input"
+              onChange={ this.handleChangeInLogin }
             />
             Senha
             <input
@@ -28,10 +73,13 @@ class Login extends Component {
               type="email"
               value={ password }
               data-testid="password-input"
+              onChange={ this.handleChangeInLogin }
             />
           </label>
           <button
             type="button"
+            disabled={ activateLogin }
+            onClick={ this.initStart }
           >
             Entrar
           </button>
@@ -41,4 +89,12 @@ class Login extends Component {
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  importLogin: (state) => dispatch(saveLoginName(state)),
+});
+
+Login.propTypes = {
+  importLogin: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,6 +1,6 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 const INITIAL_STATE = {
-  wallet: 'a',
+  wallet: '',
 };
 
 function wallet(state = INITIAL_STATE) {
