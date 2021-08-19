958446f56aa1044c2eff7644171885e183ef0d14
André Dantas <aluizcfd@gmail.com>
2021-08-19 17:30:33 -0300

    Estruturação inicial da página de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <img alt="logo Trybe" />
+        <input
+          data-testid="email-input"
+          type="email"
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+        />
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
