6df86fb48f4ca9f2fedf8b56aec0455f0322330d
Janderson Soares <jandinsm@gmail.com>
2021-08-19 22:03:52 -0300

    remove action sobrando

--- src/componentes/Table.js
+++ src/componentes/Table.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { actionExpenses, actionSunTotal } from '../actions';
+import { actionExpenses } from '../actions';
 
 class Table extends React.Component {
   constructor(props) {
