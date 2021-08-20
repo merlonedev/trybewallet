fce74cf58383d35ef9e43a0b058f26749a6d5a90
Lilian Almeida <urielbochi@gmail.com>
2021-08-19 22:39:35 -0300

    Requisito 1 completo, página de login e botão de entrar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <div>Login</div>
+        <label >
+          <input data-testid="email-input" type="text"  />
+        </label>
+        <label >
+          <input data-testid="password-input" type="password"  />
+        </label>
+        <button type="button">Entrar</button>
+      </>
+    );
+    
   }
 }
 
