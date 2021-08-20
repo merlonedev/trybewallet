1cebb50058f6b223e3e08a61ab6cf5b7c1b53629
Bernardo Coelho <bernardocezario21@gmail.com>
2021-08-20 03:01:32 -0300

    Complete Exercise 10

--- src/components/button.js
+++ src/components/button.js
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types';
 
 class Button extends React.Component {
   render() {
-    const { button, onClick, disabled } = this.props;
+    const { button, onClick, disabled, testId } = this.props;
     return (
       <div>
-        <button type="button" onClick={ onClick } disabled={ disabled }>
+        <button
+          type="button"
+          onClick={ onClick }
+          disabled={ disabled }
+          data-testId={ testId }
+        >
           { button }
         </button>
       </div>
@@ -17,7 +22,13 @@ class Button extends React.Component {
 Button.propTypes = {
   button: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
-  disabled: PropTypes.bool.isRequired,
+  disabled: PropTypes.bool,
+  testId: PropTypes.string,
+};
+
+Button.defaultProps = {
+  disabled: false,
+  testId: '',
 };
 
 export default Button;
