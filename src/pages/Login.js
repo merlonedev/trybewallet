301be70b746c94c881d37186710cdfecb892dccc
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-08-20 11:13:25 -0300

    Cria a estrutura global do estado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,51 +1,53 @@
-import React from 'react';
+import { connect } from 'react-redux';
+import { userLoginSucess } from '../actions';
 
 class Login extends React.Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       email: '',
-      password: '',
-      willredirect: false,
+      isValidEmail: false,
+    };
+    this.handleEmail = this.handleEmail.bind(this);
+    this.IsEmail = this.IsEmail.bind(this);
+  }
+
+  IsEmail(email) { // validação de email via expressão regular em: https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  handleEmail(event) {
+    const { value } = event.target;
+    const { userEmail } = this.props;
+    const { email, isValidEmail } = this.state;
+    if (this.IsEmail(value)) {
+      this.setState({ email: value, isValidEmail: true });
+    } if (isValidEmail) {
+      event.target.removeAtribute('disabled');
+      userEmail(email);
     }
   }
+
   render() {
     return (
-      <div className="login">
-        <section className="login-container">
-          <label htmlFor="email">
-            E-mail:
-            <input
-              type="text"
-              data-testid="email-input"
-              name="email"
-              value={ email }
-              onChange={()=>{}}
-            />
-          </label> 
-
-          <label htmlFor="password">
-            Senha:
-            <input
-              type="password"
-              data-testid="password-input"
-              name="password"
-              value={ password }
-              onChange={()=> {}}
-            />
-          </label>
-             
-          <button
-            type="button"
-            className="login-button"
-            onClick={()=>{}}
-          >
-            Entrar
-          </button>
-        </section>
+      <div>
+        Login
+        <input type="email" onChange={ this.handleEmail } data-testid="email-input" />
+        <input type="password" data-testid="password-input" />
+        <button type="button" disabled>Entrar</button>
       </div>
-    )
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  userEmail: (email) => dispatch(userLoginSucess(email)),
+});
+
+const mapStateToProps = ({ user: { email, isLogged } }) => ({
+  email,
+  isLogged,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
