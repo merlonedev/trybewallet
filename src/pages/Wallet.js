dd52d5f7f58e69f7c9043528bf044005526bf893
André Dantas <aluizcfd@gmail.com>
2021-08-20 00:11:11 -0300

    Adição header da página Wallet, email recuperado da Store implementado

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header>
+          <span data-testid="email-field">
+            {`Email: ${email}` }
+          </span>
+          <span data-testid="total-field">
+            Despesas Totais:
+          </span>
+          <span data-testid="header-currency-field">
+            BRL
+          </span>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Wallet);
