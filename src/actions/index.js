8206ffac444fa4eb417a03ddc645c2d6733a6c9d
André Dantas <aluizcfd@gmail.com>
2021-08-20 01:45:24 -0300

    Implementação do thunk de currencies

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,26 @@
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const REQUEST_CURRENCY = 'REQUEST_CURRENCY';
+export const RECEIVE_CURRENCY = 'RECEIVE_CURRENCY';
 
 export const saveEmailAction = (email) => ({
   type: SAVE_EMAIL,
   email,
 });
+
+const requestCurrency = () => ({
+  type: REQUEST_CURRENCY,
+});
+
+const receiveCurrency = (currency) => ({
+  type: RECEIVE_CURRENCY,
+  currency,
+});
+
+export function fetchCurrency() {
+  return (dispatch) => {
+    dispatch(requestCurrency());
+    return fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((currency) => dispatch(receiveCurrency(currency)));
+  };
+}
