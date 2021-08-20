aa54035b3a7c08212a4cfd22c9ecf4f6299221c3
lucasmulato <cobain.lucas23@gmail.com>
2021-08-20 13:00:02 -0300

    doing TableTd and re-doing index,js

---
+++ src/components/TableTd.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { deletItem } from '../actions';
+
+class TableTd extends Component {
+  constructor(props) {
+    super(props);
+    this.table = this.table.bind(this);
+    this.dispathc = this.dispathc.bind(this);
+  }
+
+  dispathc() {
+    const { wallet: { expenses } } = this.props;
+    const { submit, gastos } = this.props;
+    submit(expenses[0]);
+    gastos();
+  }
+
+  table() {
+    const { wallet: { expenses } } = this.props;
+
+    if (expenses.length > 1) {
+      const { exchangeRates } = expenses[1];
+      const array = Object.entries(exchangeRates)
+        .filter((item) => item[0] === expenses[1].currency);
+      const array2 = array[0][1].ask;
+      return (
+        <tr>
+          <td>{expenses[1].description}</td>
+          <td>{expenses[1].tag}</td>
+          <td>{expenses[1].method}</td>
+          <td>{expenses[1].value}</td>
+          <td>{array[0][1].name}</td>
+          <td>{(+array2).toFixed(2)}</td>
+          <td>{(expenses[1].value * array[0][1].ask).toFixed(2)}</td>
+          <td><p>Real</p></td>
+          <td>
+            <button
+              type="button"
+              data-testid="delete-btn"
+              onClick={ this.dispathc }
+            >
+              Delet
+            </button>
+          </td>
+        </tr>
+      );
+    }
+  }
+
+  render() {
+    const { wallet: { expenses }, counter } = this.props;
+    const { exchangeRates } = expenses[0];
+    const array = Object.entries(exchangeRates)
+      .filter((item) => item[0] === expenses[0].currency);
+    const array2 = array[counter][1].ask;
+    return (
+      <>
+        <tr>
+          <td>{expenses[counter].description}</td>
+          <td>{expenses[counter].tag}</td>
+          <td>{expenses[counter].method}</td>
+          <td>{expenses[counter].value}</td>
+          <td>{array[counter][1].name}</td>
+          <td>{(+array2).toFixed(2)}</td>
+          <td>{(expenses[counter].value * array[counter][1].ask).toFixed(2)}</td>
+          <td><p>Real</p></td>
+          <td>
+            <button
+              type="button"
+              data-testid="delete-btn"
+              onClick={ this.dispathc }
+            >
+              Delet
+            </button>
+          </td>
+        </tr>
+        { this.table() }
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  wallet: state.wallet,
+});
+
+const mapDispatchToProps = (dispathc) => ({
+  submit: (state) => dispathc(deletItem(state)),
+});
+
+TableTd.propTypes = {
+  wallet: PropTypes.shape({
+    expenses: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+  counter: PropTypes.number.isRequired,
+  submit: PropTypes.func.isRequired,
+  gastos: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TableTd);
