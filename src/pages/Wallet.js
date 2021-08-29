f62427eb0b400710189948832c617112e32f9e3e
Bruno Augusto <brunaugusto@gmail.com>
2021-08-29 02:20:22 -0300

    Update Wallet.js

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,37 +1,46 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 // import { connect } from 'react-redux';
-import Header from '../components/header';
-import FormWallet from '../components/formWallet';
+import HeaderComponent from '../components/HeaderComponent';
+import FormComponent from '../components/FormComponent';
+// import TableComponent from '../components/TableComponent';
+import HeadTable from '../components/HeadTable';
+import BodyTable from '../components/BodyTable';
 
 class Wallet extends React.Component {
-  /* constructor(props) {
-    super(props);
-    console.log(this.props);
-    const { location: { state: { user: { email } } } } = this.props;
-    this.state = {
-      email,
-    };
-  } */
+  /* constructor() {
+    super();
+    this.requestEdition = this.requestEdition.bind(this);
+    this.exitEditSection = this.exitEditSection.bind(this);
 
+     this.state = {
+      editing: false,
+      editID: 0,
+    };
+  }
+  */
+  /* requestEdition(id) {
+    this.setState({ editing: true, editID: id });
+  }
+
+  exitEditSection() {
+    this.setState({ editing: false, editID: 0 });
+  }
+  */
   render() {
+    // const { editing, editID } = this.state;
     return (
       <section>
-        <Header />
-        <FormWallet />
+        <HeaderComponent />
+        <FormComponent />
+        <section>
+          <table>
+            <HeadTable />
+            <BodyTable />
+          </table>
+        </section>
       </section>
     );
   }
 }
-
 export default Wallet;
-
-/* Wallet.propTypes = {
-  location: PropTypes.shape({
-    state: PropTypes.shape({
-      user: PropTypes.shape({
-        email: PropTypes.string.isRequired,
-      }),
-    }),
-  }).isRequired,
-}; */
