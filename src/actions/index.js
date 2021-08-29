f40971dbbfbbcb368cfdf2a3a1df7f5f5e7c60a4
Bruno Augusto <brunaugusto@gmail.com>
2021-08-29 02:15:20 -0300

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,38 +1,42 @@
 import {
-  GET_CURRENCIES,
-  GET_CURRENCIES_SUCCESS,
-  GET_CURRENCIES_ERROR,
-  GET_EMAIL,
+  SET_CURRENCIES,
+  SET_CURRENCIES_SUCCESS,
+  SET_CURRENCIES_ERROR,
+  SET_EMAIL,
   INSERT_EXPENSE,
   DELETE_EXPENSE,
+  UPDATE_EXPENSE,
+  EDIT_EXPENSE,
+  SUM_EXPENSES,
 } from './actionTypes';
 
-export const loginAction = (payload) => ({
-  type: GET_EMAIL,
+export const emailAction = (payload) => ({
+  type: SET_EMAIL,
   payload,
 });
 
-export const getCurrencies = () => ({ type: GET_CURRENCIES });
+export const setCurrencies = () => ({ type: SET_CURRENCIES });
 
-export const getCurrenciesSuccess = (payload) => ({
-  type: GET_CURRENCIES_SUCCESS,
+export const setCurrenciesSuccess = (payload) => ({
+  type: SET_CURRENCIES_SUCCESS,
   payload,
 });
 
-export const getCurrenciesError = (error) => ({
-  type: GET_CURRENCIES_ERROR,
+export const setCurrenciesError = (error) => ({
+  type: SET_CURRENCIES_ERROR,
   error,
 });
 
 export const fetchCurrencies = () => async (dispatch) => {
-  dispatch(getCurrencies());
+  dispatch(setCurrencies());
   try {
     const endpoint = 'https://economia.awesomeapi.com.br/json/all';
     const data = await fetch(endpoint);
     const dataCurrencies = await data.json();
-    dispatch(getCurrenciesSuccess(dataCurrencies));
+    dispatch(setCurrenciesSuccess(dataCurrencies));
+    return dataCurrencies;
   } catch (error) {
-    dispatch(getCurrenciesError(error));
+    dispatch(setCurrenciesError(error));
   }
 };
 
@@ -45,3 +49,26 @@ export const deleteExpense = (payload) => ({
   type: DELETE_EXPENSE,
   payload,
 });
+
+export function updateExpense(payload) {
+  return {
+    type: UPDATE_EXPENSE,
+    payload,
+  };
+}
+
+export function editExpense(payload) {
+  return {
+    type: EDIT_EXPENSE,
+    payload,
+  };
+}
+
+export const sumExpenses = () => (dispatch, getState) => {
+  const { wallet: { expenses } } = getState();
+
+  dispatch({
+    type: SUM_EXPENSES,
+    expenses,
+  });
+};
