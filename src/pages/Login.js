f459b2b0fa6e05da88557c4842693c2e32cc9b65
Bruno Augusto <brunaugusto@gmail.com>
2021-08-29 02:19:58 -0300

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,83 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { loginAction } from '../actions';
+import LoginComponent from '../components/LoginComponent';
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      disabled: true,
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleCheck = this.handleCheck.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  handleCheck() {
-    const { email, password } = this.state;
-    const parseEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-    const minLenghtPassword = 6;
-    const checkEmail = parseEmail.test(email);
-    if (checkEmail && password.length >= minLenghtPassword) {
-      this.setState({ disabled: false });
-    } else {
-      this.setState({ disabled: true });
-    }
-  }
-
   render() {
-    const { email, password, disabled } = this.state;
-    const { submitEmail } = this.props;
     return (
-      <form>
-        <input
-          type="email"
-          label="Email"
-          onChange={ this.handleChange }
-          placeholder="Digite o Email"
-          name="email"
-          value={ email }
-          onKeyUp={ this.handleCheck }
-          data-testid="email-input"
-        />
-        <input
-          label="Password"
-          onChange={ this.handleChange }
-          placeholder="Senha"
-          name="password"
-          type="password"
-          value={ password }
-          onKeyUp={ this.handleCheck }
-          data-testid="password-input"
-        />
-        <Link to="/carteira">
-          <button
-            type="button"
-            disabled={ disabled }
-            onClick={ () => submitEmail(email) }
-          >
-            Entrar
-          </button>
-        </Link>
-      </form>
-    );
+      <main>
+        <LoginComponent />
+
+      </main>);
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  submitEmail: (email) => dispatch((loginAction(email))),
-});
-
-export default connect(null, mapDispatchToProps)(Login);
-
-Login.propTypes = {
-  submitEmail: PropTypes.func.isRequired,
-};
+export default Login;
