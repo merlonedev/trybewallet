eb1e6db19e6bcd12d44422d554f542c5ed8cddf5
VictorDG00 <victord.goncalves@outlook.com>
2021-09-09 07:27:57 -0300

    req 3

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,30 +1,54 @@
 import React, { useContext } from 'react';
 import Context from '../context/Context';
 
-function Filters() {
-  const { filters, setFilters } = useContext(Context);
-  const { filterByName: { name } } = filters;
+function Table() {
+  const { data, filters: { filterByName, filterByNumericValues } } = useContext(Context);
 
-  const handleChange = ({ target: { value } }) => {
-    setFilters({
-      ...filters,
-      filterByName: {
-        name: value,
-      },
-    });
+  if (!data.length) return <span>Loading...</span>;
+
+  const list = Object.keys(data[0]);
+  // combinação do splice com indexOf baseado em:
+  // https://www.mundojs.com.br/2018/09/06/removendo-elementos-de-uma-lista-array-javascript/
+  list.splice(list.indexOf('residents'), 1);
+  const columns = list;
+
+  const handleFilter = () => {
+    let planets = [...data];
+    if (filterByName.name) {
+      planets = planets.filter(({ name }) => name.includes(filterByName.name));
+    }
+    if (filterByNumericValues.length) {
+      const comparator = {
+        'maior que': (a, b) => a > b,
+        'menor que': (a, b) => a < b,
+        'igual a': (a, b) => a === b,
+      };
+      filterByNumericValues.forEach(({ column, comparison, value }) => {
+        planets = planets
+          .filter((planet) => comparator[comparison](+planet[column], +value));
+      });
+    }
+    return planets;
   };
-
+  const fillLines = () => (
+    handleFilter().map((planet, index) => (
+      <tr key={ index }>
+        { columns.map((column, i) => <td key={ i }>{ planet[column] }</td>) }
+      </tr>
+    ))
+  );
   return (
-    <section>
-      <input
-        data-testid="name-filter"
-        type="text"
-        placeholder="Search"
-        value={ name }
-        onChange={ handleChange }
-      />
-    </section>
+    <table>
+      <thead>
+        <tr>
+          { columns.map((column, i) => <th key={ i }>{ column }</th>) }
+        </tr>
+      </thead>
+      <tbody>
+        { fillLines() }
+      </tbody>
+    </table>
   );
 }
 
-export default Filters;
+export default Table;
